30206a2efecd42b9ed0f79234ea11c13a99022eb
jamiepollock <jamie.pollock@rhythmagency.com>
2023-01-12 17:18:04 -0500

    Added IDiscoverableMapDefinition

---
+++ src/Rhythm.Umbraco.Mapping/Mapper/IDiscoverableMapDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace Rhythm.Umbraco.Mapping.Mapper;
+
+using global::Umbraco.Cms.Core.Composing;
+using global::Umbraco.Cms.Core.Mapping;
+
+/// <summary>
+/// A contract for <see cref="IMapDefinition" /> implementations that are discoverable by the type loader by default.
+/// </summary>
+public interface IDiscoverableMapDefinition : IDiscoverable, IMapDefinition
+{
+}
