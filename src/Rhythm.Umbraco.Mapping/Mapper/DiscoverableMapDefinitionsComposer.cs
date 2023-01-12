de5b9bca4021b9575a6b8681c39fcff3386e9f52
jamiepollock <jamie.pollock@rhythmagency.com>
2023-01-12 17:18:33 -0500

    Created DiscoverableMapDefinitionsComposer
    This auto registers IDiscoverableMapDefinition implementations.

---
+++ src/Rhythm.Umbraco.Mapping/Mapper/DiscoverableMapDefinitionsComposer.cs
@@ -0,0 +1,17 @@
+﻿namespace Rhythm.Umbraco.Mapping.Mapper
+{
+    using global::Umbraco.Cms.Core.Composing;
+    using global::Umbraco.Cms.Core.DependencyInjection;
+    
+    /// <summary>
+    /// A composer to register any <see cref="IDiscoverableMapDefinition"/> implementations.
+    /// </summary>
+    public sealed class DiscoverableMapDefinitionsComposer : IComposer
+    {
+        /// <inheritdoc />
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.MapDefinitions().Add(builder.TypeLoader.GetTypes<IDiscoverableMapDefinition>());
+        }
+    }
+}
