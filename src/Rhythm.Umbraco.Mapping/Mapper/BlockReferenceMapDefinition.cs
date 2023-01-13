a0587f4d191dccaae7ee67eafcde8285e3458ead
jamiepollock <jamie.pollock@rhythmagency.com>
2023-01-13 11:03:01 -0500

    Added PageComponentMapDefinition base classes
    This is to allow for leverage mapping these inputs to non-IPageComponentModel objects.

---
+++ src/Rhythm.Umbraco.Mapping/Mapper/BlockReferenceMapDefinition.cs
@@ -0,0 +1,68 @@
+﻿namespace Rhythm.Umbraco.Mapping.Mapper;
+
+using global::Umbraco.Cms.Core.Mapping;
+using global::Umbraco.Cms.Core.Models.Blocks;
+using global::Umbraco.Cms.Core.Models.PublishedContent;
+using Rhythm.Models.Common;
+
+/// <summary>
+/// Maps a <see cref="IBlockReference"/> with a <typeparamref name="TContent"/>.
+/// </summary>
+/// <typeparam name="TContent">The type of the content.</typeparam>
+/// <typeparam name="TOutput">THe type of the output.</typeparam>
+/// <remarks>This map definition ignores the settings. Use the <see cref="BlockReferenceMapDefinition{TContent, TSettings, TOutput}"/> if settings are needed.</remarks>
+public abstract class BlockReferenceMapDefinition<TContent, TOutput> : IDiscoverableMapDefinition
+    where TContent : IPublishedElement
+{
+    /// <inheritdoc />
+    public virtual void DefineMaps(IUmbracoMapper mapper)
+    {
+        mapper.Define<BlockListItem<TContent>, TOutput?>((block, mapperContext) => this.Map(block, mapperContext));
+    }
+
+    /// <summary>
+    /// Maps the model to a <typeparamref name="TOutput"/>.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <param name="mapperContext">The mapper context.</param>
+    /// <returns>A <typeparamref name="TOutput"/>.</returns>
+    protected virtual TOutput? Map(BlockListItem<TContent> model, MapperContext mapperContext)
+    {
+        return Map(model.Content, mapperContext);
+    }
+
+    /// <inheritdoc />
+    public abstract TOutput? Map(TContent content, MapperContext mapperContext);
+}
+
+/// <summary>
+/// Maps a <see cref="IBlockReference"/> with a <typeparamref name="TContent"/> and <typeparamref name="TSettings"/>.
+/// </summary>
+/// <typeparam name="TContent">The type of the content.</typeparam>
+/// <typeparam name="TSettings">The type of the settings.</typeparam>
+/// <typeparam name="TOutput">THe type of the output.</typeparam>
+/// <remarks>This map definition uses settings. Use the <see cref="BlockReferenceMapDefinition{TContent, TOutput}"/> if settings are not needed.</remarks>
+public abstract class BlockReferenceMapDefinition<TContent, TSettings, TOutput> : IDiscoverableMapDefinition 
+    where TContent : IPublishedElement
+    where TSettings : IPublishedElement
+{
+    /// <inheritdoc />
+    public virtual void DefineMaps(IUmbracoMapper mapper)
+    {
+        mapper.Define<BlockListItem<TContent, TSettings>, TOutput?>((block, mapperContext) => this.Map(block, mapperContext));
+    }
+
+    /// <summary>
+    /// Maps the model to a <typeparamref name="TOutput"/>.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <param name="mapperContext">The mapper context.</param>
+    /// <returns>A <typeparamref name="TOutput"/>.</returns>
+    protected virtual TOutput? Map(BlockListItem<TContent, TSettings> model, MapperContext mapperContext)
+    {
+        return Map(model.Content, model.Settings, mapperContext);
+    }
+
+    /// <inheritdoc />
+    public abstract TOutput? Map(TContent content, TSettings settings, MapperContext mapperContext);
+}
--- src/Rhythm.Umbraco.Mapping/Mapper/BlockReferencePageComponentMapDefinition.cs
+++ src/Rhythm.Umbraco.Mapping/Mapper/BlockReferencePageComponentMapDefinition.cs
@@ -5,60 +5,25 @@ using global::Umbraco.Cms.Core.Models.Blocks;
 using global::Umbraco.Cms.Core.Models.PublishedContent;
 using Rhythm.Models.Common;
 
-/// <summary>
-/// Maps a <see cref="IBlockReference"/> with a <typeparamref name="TContent"/>.
-/// </summary>
-/// <typeparam name="TContent">The type of the content.</typeparam>
-/// <remarks>This map definition ignores the settings. Use the <see cref="BlockReferencePageComponentMapDefinition{TContent, TSettings}"/> if settings are needed.</remarks>
-public abstract class BlockReferencePageComponentMapDefinition<TContent> : IDiscoverableMapDefinition
+/// <inheritdoc />
+public abstract class BlockReferencePageComponentMapDefinition<TContent> : BlockReferenceMapDefinition<TContent, IPageComponentModel>
     where TContent : IPublishedElement
 {
-    public void DefineMaps(IUmbracoMapper mapper)
+    /// <inheritdoc />
+    public override void DefineMaps(IUmbracoMapper mapper)
     {
         mapper.Define<BlockListItem<TContent>, IPageComponentModel?>((block, mapperContext) => this.Map(block, mapperContext));
     }
-
-    /// <summary>
-    /// Maps the model to a <see cref="IPageComponentModel"/>.
-    /// </summary>
-    /// <param name="model">The model.</param>
-    /// <param name="mapperContext">The mapper context.</param>
-    /// <returns>A <see cref="IPageComponentModel"/>.</returns>
-    protected virtual IPageComponentModel? Map(BlockListItem<TContent> model, MapperContext mapperContext)
-    {
-        return Map(model.Content, mapperContext);
-    }
-
-    /// <inheritdoc />
-    public abstract IPageComponentModel? Map(TContent content, MapperContext mapperContext);
 }
 
-/// <summary>
-/// Maps a <see cref="IBlockReference"/> with a <typeparamref name="TContent"/> and <typeparamref name="TSettings"/>.
-/// </summary>
-/// <typeparam name="TContent">The type of the content.</typeparam>
-/// <typeparam name="TSettings">The type of the settings.</typeparam>
-/// <remarks>This map definition uses settings. Use the <see cref="BlockReferencePageComponentMapDefinition{TContent}"/> if settings are not needed.</remarks>
-public abstract class BlockReferencePageComponentMapDefinition<TContent, TSettings> : IDiscoverableMapDefinition 
+/// <inheritdoc />
+public abstract class BlockReferencePageComponentMapDefinition<TContent, TSettings> : BlockReferenceMapDefinition<TContent, TSettings, IPageComponentModel>
     where TContent : IPublishedElement
     where TSettings : IPublishedElement
 {
-    public void DefineMaps(IUmbracoMapper mapper)
+    /// <inheritdoc />
+    public override void DefineMaps(IUmbracoMapper mapper)
     {
         mapper.Define<BlockListItem<TContent, TSettings>, IPageComponentModel?>((block, mapperContext) => this.Map(block, mapperContext));
     }
-
-    /// <summary>
-    /// Maps the model to a <see cref="IPageComponentModel"/>.
-    /// </summary>
-    /// <param name="model">The model.</param>
-    /// <param name="mapperContext">The mapper context.</param>
-    /// <returns>A <see cref="IPageComponentModel"/>.</returns>
-    protected virtual IPageComponentModel? Map(BlockListItem<TContent, TSettings> model, MapperContext mapperContext)
-    {
-        return Map(model.Content, model.Settings, mapperContext);
-    }
-
-    /// <inheritdoc />
-    public abstract IPageComponentModel? Map(TContent content, TSettings settings, MapperContext mapperContext);
 }
