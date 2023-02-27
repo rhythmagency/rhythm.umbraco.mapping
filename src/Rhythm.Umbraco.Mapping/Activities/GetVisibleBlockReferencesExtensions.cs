5df91ded0adae27c32c287c8bf378d7f53f33638
jamiepollock <jamie.pollock@rhythmagency.com>
2023-02-27 12:08:24 -0500

    Added an extension to GetVisibleBlockReferences
    This supports returning a BlockListModel is a block list model is passed in.

---
+++ src/Rhythm.Umbraco.Mapping/Activities/GetVisibleBlockReferencesExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Rhythm.Umbraco.Mapping.Activities;
+
+using global::Umbraco.Cms.Core.Models.Blocks;
+
+/// <summary>
+/// Extension methods that augments the <see cref="IGetVisibleBlockReferences" /> interface.
+/// </summary>
+public static class GetVisibleBlockReferencesExtensions
+{
+	public static BlockListModel Execute(this IGetVisibleBlockReferences activity, BlockListModel? blocks)
+	{
+		var visibleBlocks = activity.Execute(blocks);
+
+		return new BlockListModel(visibleBlocks.ToArray());
+	}
+}
